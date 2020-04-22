026cf7c3c6bdf594797315e352598d011eb957a1
songofhawk <songofhawk@gmail.com>
2020-04-22 23:02:46 +0800

    换logo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.jpg">
+        <SvgRender :title="svgData.title"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import SvgRender from './components/SvgRender.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            SvgRender
+        },
+        data() {
+            return {
+                svgData: {
+                    title: 'something happens'
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
+{
+title: String,
+likes: Number
+}
---
+++ src/components/SvgRender.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <label> title:
+            <input v-model="title"/>
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SvgRender',
+        props: {
+            title:String,
+        },
+    }
+</script>
+
+<style>
+
+</style>
